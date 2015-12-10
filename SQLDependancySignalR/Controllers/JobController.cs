1a3ff91e4b4374f0afb428bfe0936e932e33fec0
diggudg@gmail.com <Dnsbharti#123>
2015-12-10 15:38:23 +0530

    hub added

---
+++ SQLDependancySignalR/Controllers/JobController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SQLDependancySignalR.Controllers
+{
+    
+    public class JobController : ApiController
+    {
+        JobInfoRepository objRepo = new JobInfoRepository();
+        [Route("Job")]
+        public IEnumerable<JobInfo> Get()
+        {
+            return objRepo.GetData();
+        }
+    }
+}
---
+++ SQLDependancySignalR/JobInfoRepository.cs
@@ -0,0 +1,81 @@
+﻿using SQLDependancySignalR.Hubs;
+using SQLDependancySignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SQLDependancySignalR
+{
+    public class JobInfoRepository
+    {
+        SignalREntities sr = new SignalREntities();
+
+        public IEnumerable<JobInfo> GetData()
+        {
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaulConnection"].ConnectionString))
+            {
+                connection.Open();
+                string a = @"SELECT COUNT(*) as Votes,UserId,Band  FROM [dbo].[Vote] group by UserId,Band order by Votes desc";
+                //using (SqlCommand command = new SqlCommand(@"SELECT COUNT(*) as Votes,UserId,Band  FROM [dbo].[Vote] group by UserId,Band order by Votes desc", connection))
+                //using (SqlCommand command = new SqlCommand(@"SELECT [Id],[UserId],[Band] FROM [dbo].[Vote] ", connection))
+                using (SqlCommand command = new SqlCommand(a, connection))
+                {
+                    // Make sure the command object does not already have
+                    // a notification object associated with it.using 
+                    //(SqlCommand command = new SqlCommand(@"SELECT [JobID],[Name],[LastExecutionDate],[Status]
+                   // FROM[dbo].[JobInfo]", connection))
+                    command.Notification = null;
+
+                    SqlDependency dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(dependency_OnChange);
+
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                        return reader.Cast<IDataRecord>()
+                            .Select(x => new JobInfo()
+                            {
+                                JobID = x.GetInt32(0),
+                                Name = x.GetInt32(1),
+                                LastExecutionDate = x.GetString(2),
+                               // Status= x.GetString(3)
+                            }).ToList();
+
+
+
+                }
+            }
+        }
+        public List<FinalVoting> GetJob()
+        {
+            List<FinalVoting> a = new List<FinalVoting>();
+            return a;
+        }
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            JobHub.Show();
+        }
+
+
+    }
+
+    public class JobInfo
+    {
+        public int JobID { get; set; }
+        public int Name { get; set; }
+        public string LastExecutionDate { get; set; }
+        public string Status { get; set; }
+    }
+    public class FinalVoting
+    {
+        public string Name  { get; set; }
+        public int TotalVotes { get; set; }
+        public string EmpCode { get; set; }
+    }
+}
